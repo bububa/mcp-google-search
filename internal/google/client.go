6d6713675bf03ba773badd2f1cbd25a8e3aa9efc
Syd Xu <prof.syd.xu@gmail.com>
2025-07-07 13:08:39 +0800

    fix(google): http client

--- internal/google/client.go
+++ internal/google/client.go
@@ -3,11 +3,13 @@ package google
 import (
 	"bytes"
 	"context"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"os"
 )
 
 type Client struct {
@@ -17,10 +19,19 @@ type Client struct {
 }
 
 func NewClient(cx string, key string) *Client {
+	httpClient := http.DefaultClient
+	if proxyURL := os.Getenv("https_proxy"); proxyURL != "" {
+		proxy, _ := url.Parse(proxyURL)
+		httpClient.Transport = &http.Transport{
+			Proxy:           http.ProxyURL(proxy),
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		fmt.Println("USE https_proxy", proxyURL)
+	}
 	return &Client{
 		cx:         cx,
 		key:        key,
-		httpClient: http.DefaultClient,
+		httpClient: httpClient,
 	}
 }
 
@@ -40,11 +51,11 @@ func (c *Client) Search(ctx context.Context, req *Request, resp *Response) error
 	buf.WriteString(GATEWAY)
 	buf.WriteByte('?')
 	buf.WriteString(values.Encode())
-	fmt.Println(buf.String())
 	httpReq, err := http.NewRequestWithContext(ctx, http.MethodGet, buf.String(), nil)
 	if err != nil {
 		return err
 	}
+	httpReq.Header.Set("Content-Type", "application/json")
 	httpResp, err := c.httpClient.Do(httpReq)
 	if err != nil {
 		return err
@@ -54,8 +65,7 @@ func (c *Client) Search(ctx context.Context, req *Request, resp *Response) error
 		body, _ := io.ReadAll(httpResp.Body)
 		return fmt.Errorf("google search API returned an error: %s, \n%s", httpResp.Status, string(body))
 	}
-	buf.Reset()
-	if err := json.NewDecoder(&buf).Decode(resp); err != nil {
+	if err := json.NewDecoder(httpResp.Body).Decode(resp); err != nil {
 		return err
 	}
 	return nil
